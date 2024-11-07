3c7d3ce30ba7cdcd96cc0d72ce565d4826a62a5a
nahleyed <168160291+nahleyed@users.noreply.github.com>
2024-11-07 01:20:54 -0800

    login Updade

---
+++ index.js
@@ -0,0 +1,11 @@
+const seven=document.getElementById('seven');
+const nine=document.getElementById('eight');
+const del=document.getElementById('del');
+const four=document.getElementById('four');
+const five=document.getElementById('five');
+const equal=document.getElementById('equal');
+const plus=document.getElementById('plus');
+
+del.onClick()=function myFunc(){
+    mytext.textcontent=0;
+}
